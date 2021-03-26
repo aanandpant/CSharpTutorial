8e93658b4512c8930bb3bb00d3abea06d30168d4
Aanand Pant (cpant) <cpant@micron.com>
2021-03-26 11:09:53 +0800

    Initial commit of the project

---
+++ CsharpTutorial/CsharpTutorial/PointsAndLines/point.cs
@@ -0,0 +1,11 @@
+﻿/*namespaces helps you to organize class files*/
+
+namespace PointsAndLines
+{
+    class point
+    {
+        public int x;
+        public int y;
+    }
+}
+
---
+++ CsharpTutorial/CsharpTutorial/Program.cs
@@ -0,0 +1,27 @@
+﻿/* list of namespaces*/
+using System;
+using PointsAndLines;
+
+namespace CsharpTutorial
+{
+
+    /*
+    class point
+    {
+        public int x;
+        public int y;
+    }*/
+    class testCode
+    {
+        /*static keyword is used to call a method before class instantiation, which means that you can call a function without creating the object of that class.*/
+        static void Main()
+        {
+            point point = new point();
+            int x=point.x = 2;
+            int y=point.y = 3;
+            Console.WriteLine("value of x=" + x + " and the value of Y=" + y);
+            Console.ReadLine();
+        }
+
+    }
+}
